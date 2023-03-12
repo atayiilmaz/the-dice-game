515051eaebe5f5135232021b47578fa3788d7161
Ata Berk Yılmaz <126782062+atayiilmaz@users.noreply.github.com>
2023-03-12 18:47:46 +0300

    first version

---
+++ js/index.js
@@ -0,0 +1,45 @@
+//for player1
+
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+switch(randomNumber1){
+    case 1: document.querySelector("img.img1").setAttribute("src", "./images/dice1.png");
+        break;
+    case 2: document.querySelector("img.img1").setAttribute("src", "./images/dice2.png");
+        break;
+    case 3: document.querySelector("img.img1").setAttribute("src", "./images/dice3.png");
+        break;
+    case 4: document.querySelector("img.img1").setAttribute("src", "./images/dice4.png");
+        break;
+    case 5: document.querySelector("img.img1").setAttribute("src", "./images/dice5.png");
+        break;
+    case 6: document.querySelector("img.img1").setAttribute("src", "./images/dice6.png");
+}
+
+//for player2
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+switch(randomNumber2){
+    case 1: document.querySelector("img.img2").setAttribute("src", "./images/dice1.png");
+        break;
+    case 2: document.querySelector("img.img2").setAttribute("src", "./images/dice2.png");
+        break;
+    case 3: document.querySelector("img.img2").setAttribute("src", "./images/dice3.png");
+        break;
+    case 4: document.querySelector("img.img2").setAttribute("src", "./images/dice4.png");
+        break;
+    case 5: document.querySelector("img.img2").setAttribute("src", "./images/dice5.png");
+        break;
+    case 6: document.querySelector("img.img2").setAttribute("src", "./images/dice6.png");
+}
+
+//decide the winner
+
+if(randomNumber1 === randomNumber2){
+    document.querySelector("h1").textContent = "Draw";
+} else if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").textContent = "Player 1 Wins!";
+} else {
+    document.querySelector("h1").textContent = "Player 2 Wins!";
+}
