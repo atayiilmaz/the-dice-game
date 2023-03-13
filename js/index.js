a11b650df6d84362b8fe663343023b35ad6c3f9d
Ata Berk Yılmaz <126782062+atayiilmaz@users.noreply.github.com>
2023-03-13 12:43:42 +0300

    adding comment lines and new scripts.

--- js/index.js
+++ js/index.js
@@ -2,7 +2,14 @@
 
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
-switch(randomNumber1){
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+
+var randomImgSource = "images/" + randomDiceImage;
+
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src",randomImgSource);
+
+/*switch(randomNumber1){
     case 1: document.querySelector("img.img1").setAttribute("src", "./images/dice1.png");
         break;
     case 2: document.querySelector("img.img1").setAttribute("src", "./images/dice2.png");
@@ -14,13 +21,17 @@ switch(randomNumber1){
     case 5: document.querySelector("img.img1").setAttribute("src", "./images/dice5.png");
         break;
     case 6: document.querySelector("img.img1").setAttribute("src", "./images/dice6.png");
-}
+}*/
 
 //for player2
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-switch(randomNumber2){
+var randomImgSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src",randomImgSource2);
+
+/*switch(randomNumber2){
     case 1: document.querySelector("img.img2").setAttribute("src", "./images/dice1.png");
         break;
     case 2: document.querySelector("img.img2").setAttribute("src", "./images/dice2.png");
@@ -32,7 +43,7 @@ switch(randomNumber2){
     case 5: document.querySelector("img.img2").setAttribute("src", "./images/dice5.png");
         break;
     case 6: document.querySelector("img.img2").setAttribute("src", "./images/dice6.png");
-}
+}*/
 
 //decide the winner
 
